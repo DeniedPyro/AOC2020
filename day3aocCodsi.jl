e421d2e7da2d84da734fa900beb6d64f57bd3ad1
Julien Codsi <julien.codsi@umontreal.ca>
2020-12-04 04:35:20 -0500

    Mine is now way shorter than yours

--- day3aocCodsi.jl
+++ day3aocCodsi.jl
@@ -1,3 +1,11 @@
-bruh=readlines("day3i.txt")
-sum(bruh[i][mod1(3(i-1)+1,length(bruh[1]))]=='#' for i in 1:length(bruh))
+# part 1
+b=readlines("c")
+sum(b[i][mod1(3i-2,length(b[1]))]∈'#' for i∈1:length(b))
+
+# part 2
+b=readlines("c")
+prod(sum(b[i][mod1(j,length(b[1]))]∈'#' for (i,j)∈zip(1:y:length(b),1:x:y*length(b)x)) for (x,y)∈zip([1,3,5,7,1],[1,1,1,1,2]))
+
+b,~=readlines("c"),length
+prod(sum(b[i][mod1(j,~(b[1]))]∈'#' for (i,j)∈zip(1:y:~b,1:x:y*x*~b)) for (x,y)∈zip([1,3,5,7,1],[1,1,1,1,2]))
 
